5cc80b922d75c9ed5c4f1e399eb0840659ed188c
gabrieldaniellanza <gabrieldaniel.lanza@gmail.com>
2022-08-17 16:22:05 -0300

    Update practice3.py

--- practice/python/section.5/practice3.py
+++ practice/python/section.5/practice3.py
@@ -1,1 +1,14 @@
+import math
 
+#Escribe una función que pueda decirte si un año (número entero) es bisiesto o no.
+def EsBisiesto(anio):
+  if (anio % 4) == 0:
+    if (anio % 100 == 0):
+      return(anio % 400 == 0)
+    else:
+      return True
+
+print("10 es bisiesto: " + str(EsBisiesto(10)))
+print("12 es bisiesto: " + str(EsBisiesto(12)))
+print("1800 es bisiesto: " + str(EsBisiesto(1800)))
+print("2000 es bisiesto: " + str(EsBisiesto(2000)))
