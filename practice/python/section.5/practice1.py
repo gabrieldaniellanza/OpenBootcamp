48b9a431d18ef5af4b531381da6b3071808aee52
gabrieldaniellanza <gabrieldaniel.lanza@gmail.com>
2022-08-17 15:34:07 -0300

    Update practice1.py

--- practice/python/section.5/practice1.py
+++ practice/python/section.5/practice1.py
@@ -1,1 +1,16 @@
+import math
 
+# Escribe una función que calcule el área de un triángulo, recibiendo la altura y la base como parámetros 
+def CalularAreaTriangulo(altura, base):
+  area = base * altura / 2
+  print("Area Triangulo: " + str(area))
+
+
+# y otra función que calcule el área de un círculo recibiendo el radio del mismo.
+def CalularAreaCirculo(radio):
+  area = 2 * math.pi * radio
+  print("Area Circulo: " + str(area))
+  
+
+CalularAreaTriangulo(2,3)
+CalularAreaCirculo(5)
