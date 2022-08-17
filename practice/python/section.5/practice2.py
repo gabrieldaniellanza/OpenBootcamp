6fdddb9fb68373e10f0fe641850d41b70e9020fa
gabrieldaniellanza <gabrieldaniel.lanza@gmail.com>
2022-08-17 16:05:41 -0300

    Update practice2.py

--- practice/python/section.5/practice2.py
+++ practice/python/section.5/practice2.py
@@ -1,1 +1,10 @@
+import math
 
+def EsPrimo(numero):
+  for x in range(2, math.floor(math.sqrt(numero))):
+    if (numero % x) == 0 :
+      return False
+  return True  
+
+print("19 es primo: " + str(EsPrimo(19)))
+print("18 es primo: " + str(EsPrimo(18)))
