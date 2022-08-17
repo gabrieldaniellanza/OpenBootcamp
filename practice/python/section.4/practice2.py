870fe8e9047c0175393e1fb5d388af185279c679
gabrieldaniellanza <gabrieldaniel.lanza@gmail.com>
2022-08-17 14:56:17 -0300

    Update practice2.py

--- practice/python/section.4/practice2.py
+++ practice/python/section.4/practice2.py
@@ -1,1 +1,8 @@
 
+#Escribe un programa capaz de mostrar todos los números impares desde un número de inicio y otro final.
+desde = int(input('Ingrese el numero desde: '))
+hasta = int(input('Ingrese el numero desde: '))
+
+for x in range(desde, (hasta +1)):
+   if(x % 2 == 1):
+      print(str(x))
