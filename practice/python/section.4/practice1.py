951d640659bcc98947f86c78c8798e6b62e9ec33
gabrieldaniellanza <gabrieldaniel.lanza@gmail.com>
2022-08-17 14:43:44 -0300

    Update practice1.py

--- practice/python/section.4/practice1.py
+++ practice/python/section.4/practice1.py
@@ -1,1 +1,9 @@
+#Escribe un programa que pregunte al usuario su edad 
+edad = int(input('¿Cuál es su edad?: '))
 
+esMayor = ''
+if edad < 18 :
+  esMayor = 'no '
+
+#muestre por pantalla si es mayor de edad o no.
+print('Usted ' + esMayor + 'es mayor de edad')
